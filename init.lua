e365eb1ffe3a0db1d10b55f07929b4a307c25049
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-06 02:10:52 -0400

    update init.lua

--- init.lua
+++ init.lua
@@ -6,24 +6,5 @@ vim.g.maplocalleader = " "
 -- syntax: mode, keys, command
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
 
-
--- LINE NUMBER
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-
--- INDENTATION
--- tabs converted to spaces
-vim.opt.expandtab = true 
--- shift indentation size
-vim.opt.shiftwidth = 2
--- 1 tab = 2 spaces
-vim.opt.tabstop = 2
-
-
--- SEARCH
-vim.opt.ignorecase = true
--- override ignore case settings if a capital letter is included
-vim.opt.smartcase = true
-
-
+-- DEPENDENCIES
+require("options")
