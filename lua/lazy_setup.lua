dfbc01ed1413ae639ff238460ceb1814b02caeba
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-06 03:27:28 -0400

    add colorscheme

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,15 +1,28 @@
 -- lua/lazy_setup.lua
 
 -- lazy.nvim install directory
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
 
 -- if lazy.nvim isn't installed
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 -- add it to the runtime path
 vim.opt.rtp:prepend(lazypath)
 
 -- start
-require("lazy").setup({ "folke/tokyonight.nvim", })
+require("lazy").setup({
+	"folke/tokyonight.nvim",
+})
+
+
+-- COLORSCHEME
+vim.cmd.colorscheme "tokyonight"
