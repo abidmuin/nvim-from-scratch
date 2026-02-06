4a89c22f5704adae8122547d354b55a3eb6baa1e
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-06 01:47:08 -0400

    update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
--- line number
+-- LINE NUMBER
 vim.opt.number = true
--- relative line number
 vim.opt.relativenumber = true
 
 -- INDENTATION
-vim.opt.expandtab = true
+-- tabs converted to spaces
+vim.opt.expandtab = true 
+-- shift indentation size
 vim.opt.shiftwidth = 2
+-- 1 tab = 2 spaces
 vim.opt.tabstop = 2
+
