0f92af3383153d547f65c56753b0d6771d730de9
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-06 01:40:16 -0400

    update init.lua

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- line number
+vim.opt.number = true
+-- relative line number
+vim.opt.relativenumber = true
+
+-- INDENTATION
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
