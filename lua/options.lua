9493df0f931e7389fe657f3fd77c1ca0ee5b27f1
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-06 02:11:32 -0400

    add options.lua

---
+++ lua/options.lua
@@ -0,0 +1,20 @@
+-- lua/options.lua
+
+-- LINE NUMBER
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+
+-- INDENTATION
+-- tabs converted to spaces
+vim.opt.expandtab = true
+-- shift indentation size
+vim.opt.shiftwidth = 2
+-- 1 tab = 2 spaces
+vim.opt.tabstop = 2
+
+
+-- SEARCH
+vim.opt.ignorecase = true
+-- override ignore case settings if a capital letter is included
+vim.opt.smartcase = true
