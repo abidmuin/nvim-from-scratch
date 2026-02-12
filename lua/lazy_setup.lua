6cf0726b58d9a322601d0252856b181eef9f2576
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-12 00:29:45 -0400

    add telescope

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -37,7 +37,25 @@ require("lazy").setup({
 			indent = { enable = true },
 		})
 		end
-	}
+	},
+	-- TELESCOPE: Fuzzy Finder
+	{
+		"nvim-telescope/telescope.nvim", tag='0.1.8',
+		dependencies = {'nvim-lua/plenary.nvim'},
+		config = function()
+			local builtin = require('telescope.builtin')
+			-- KEYMAPS
+			-- Find Files
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			-- Find Grep (Text)
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			-- Find Buffers
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			-- Find Help
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {}) 
+end
+
+	},
 })
 
 
