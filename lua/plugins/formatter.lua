8e4b2d8cf6807ce9536c5a40fb56dd2fd529e147
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-12 10:40:27 -0400

    modularize formatter.lua

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,31 @@
+-- lua/plugins/formatter.lua
+
+return {
+	"stevearc/conform.nvim",
+
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang-format" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		-- MANUAL TRIGGER: <Space>mp to "Make Pretty"
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
