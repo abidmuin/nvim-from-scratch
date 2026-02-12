c4d1a70f9e8e707a67f7df5c746a10813bafc91e
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-12 01:22:25 -0400

    add LSP and Autocompletion

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -53,9 +53,72 @@ require("lazy").setup({
 			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 			-- Find Help
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {}) 
-end
-
+		end
 	},
+-- LSP Support (Safe Mode)
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      -- "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      -- 1. Setup Mason (The installer)
+      require("mason").setup()
+
+      -- CRITICAL CHANGE: We are SKIPPING mason-lspconfig.setup() 
+      -- because it is currently crashing your Neovim.
+
+      -- 2. Setup the Servers Manually
+      local lspconfig = require("lspconfig")
+
+      -- Setup Lua Server
+      lspconfig.lua_ls.setup({
+      	capabilities = capabilities,
+      	cmd = {"/home/abidmuin/.local/share/nvim/mason/bin/lua-language-server"},
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
+
+      -- Setup C Server
+      -- lspconfig.clangd.setup({})
+    end,
+  },
+  -- AUTOCOMPLETION
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",     -- Source for LSP
+      "L3MON4D3/LuaSnip",         -- Snippet Engine
+      "saadparwaiz1/cmp_luasnip", -- Source for Snippets
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        -- 1. Snippet Engine (Required)
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        -- 2. Key Mappings
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(), -- Manually trigger menu
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Enter to select
+        }),
+        -- 3. Sources (Where words come from)
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' }, -- From the LSP (Smart code)
+          { name = 'luasnip' },  -- Snippets
+        }, {
+          { name = 'buffer' },   -- Text in the current file
+        })
+      })
+    end,
+  },
 })
 
 
