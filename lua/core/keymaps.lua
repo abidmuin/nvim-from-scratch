e6d6434be2f7a9ced856de5fefcb0efebcc1ceed
abidmuin <21988951+abidmuin@users.noreply.github.com>
2026-02-18 03:16:07 -0400

    add go run keybind

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,3 +7,13 @@ vim.g.maplocalleader = " "
 
 -- syntax: mode, keys, command
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
+
+-- START_GO
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "go",
+	callback = function()
+		vim.keymap.set("n", "<leader>gr", ":!go run %<CR>", { buffer = true, desc = "Go Run" })
+	end,
+})
+
+-- END_GO
